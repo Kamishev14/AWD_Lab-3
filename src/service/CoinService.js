a571b0632c69cd838ecc7ce4decbeef5ad187aaf
Kristijan <kamishevkristijan@gmail.com>
2025-12-04 01:06:42 +0100

    Initial Request 1

---
+++ src/service/CoinService.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import API_KEY from "./key.js";
+
+class CoinService {
+    static getCoins() {
+        return axios.get(`https://rest.coincap.io/v3/assets?apiKey=${API_KEY}`);
+    }
+}
+
+export default CoinService;
